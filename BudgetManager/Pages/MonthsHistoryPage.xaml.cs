794932ef6655bd608240b0112a205005dc0a7112
Adrian Frydmański <adrian.frydmanski@hotmail.com>
2022-02-21 22:16:44 +0100

    History page works

--- BudgetManager/Pages/MonthsHistoryPage.xaml.cs
+++ BudgetManager/Pages/MonthsHistoryPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.UI.Xaml;
+﻿using BudgetManager.Models;
+using LiveChartsCore;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -7,6 +11,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -27,9 +32,93 @@ namespace BudgetManager.Pages
 
         string IPageWithInfo.header { get => header; set => header = value; }
 
+        private readonly static int WidthOfMonth = 100;
+        private readonly static int nOfMonths = AppData.months.Count - 1;  // omit last (current) month
+
+        public IEnumerable<ISeries> Series { get; set; }
+        public IEnumerable<ICartesianAxis> XAxis { get; set; }
+            = new Axis[] {
+                new Axis
+                {
+                    Labels = GetLabels(),
+                    LabelsRotation = 0,
+                }
+            };
+        public IEnumerable<ICartesianAxis> YAxis { get; set; }
+            = new Axis[] {
+                new Axis
+                {
+                    Position = LiveChartsCore.Measure.AxisPosition.End,
+                    Labeler = value => Math.Round(value).ToString()
+                }
+            };
+
         public MonthsHistoryPage()
         {
             this.InitializeComponent();
+            ChangeGridWidth();
+            Plot();
+        }
+
+        private void ChangeGridWidth()
+        {
+            HistoryChartGrid.Width = WidthOfMonth * AppData.months.Count - 1;
+        }
+
+        private void Plot()
+        {
+            Series = GetSeries();
+        }
+
+        private static string[] GetLabels()
+        {
+            var labels = new string[nOfMonths];
+            for (var i = 0; i < nOfMonths; i++)
+            {
+                labels[i] = AppData.months.ElementAt(i).StartDate.ToString("dd.MM") + "-" + AppData.months.ElementAt(i).EndDate.ToString("dd.MM");
+            }
+            return labels;
+        }
+
+        private ObservableCollection<ISeries> GetSeries()
+        {
+            // incomes, expenses and balances
+            var monthIncomes = new double[nOfMonths];
+            var monthExpenses = new double[nOfMonths];
+            var monthBalances = new double[nOfMonths];
+            for (var i = 0; i < nOfMonths; i++)
+            {
+                monthIncomes[i] = (double)(AppData.months.ElementAt(i).GetSumOfIncomes());
+                monthExpenses[i] = (double)AppData.months.ElementAt(i).GetSumOfExpenses();
+                monthBalances[i] = Math.Round(monthIncomes[i] - monthExpenses[i], 2);
+            }
+
+            var series = new ObservableCollection<ISeries>
+            {
+                new LineSeries<double>
+                {
+                    Values = monthIncomes,
+                    Fill = null,
+                    Name = "Suma przychodów"
+                },
+                new LineSeries<double>
+                {
+                    Values = monthExpenses,
+                    Fill = null,
+                    Name = "Suma wydatków"
+                },
+                new ColumnSeries<double>
+                {
+                    Values = monthBalances,
+                    Name = "Bilans",
+                }
+            };
+            return series;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            HistoryScrollViewer.ScrollToHorizontalOffset(HistoryScrollViewer.ScrollableWidth);
         }
     }
 }
